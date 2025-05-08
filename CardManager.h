a41250f61258e364d8be27786b1fb7b3cb2f35ea
Elementlover <reubenrodriguez825@gmail.com>
2025-05-08 00:43:21 -0600

    Updated CardManager to have card creation helpers

--- CardManager.h
+++ CardManager.h
@@ -1,18 +1,20 @@
 #pragma once
 
-#include <string>
-#include <vector>
 #include <memory>
 #include <unordered_map>
+#include <string>
+#include <vector>
 #include <json.hpp>
-
-// Forward declarations to avoid circular includes
-class Card;
+#include "Card.h"
 
 class CardManager {
 public:
-    static std::shared_ptr<Card> createCardFromJson(const nlohmann::json& data); // TODO find why this uses a shared pointer and not a unique pointer
+    static std::shared_ptr<Card> createCardFromJson(const nlohmann::json& data);
 
 private:
-    static std::unordered_map<std::string, int> extractEffects(const nlohmann::json& effectData); // Function to pull effects from JSON data
+    static std::shared_ptr<Card> createEquipmentCard(const nlohmann::json& data);
+    static std::shared_ptr<Card> createCurseCard(const nlohmann::json& data);
+    static std::shared_ptr<Card> createMonsterCard(const nlohmann::json& data);
+
+    static std::unordered_map<std::string, int> extractEffects(const nlohmann::json& effectData);
 };
