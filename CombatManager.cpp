e3946e1327cbd8a0212b8a8f491f5c5446a9a806
Elementlover <reubenrodriguez825@gmail.com>
2025-05-08 12:16:16 -0600

    Dramic Pauses for added flair

--- CombatManager.cpp
+++ CombatManager.cpp
@@ -1,13 +1,18 @@
 #include "CombatManager.h"
 #include <iostream>
 #include <random>
+#include <thread>  // For sleep functionality
+#include <chrono>  // For time duration
 
 void CombatManager::resolveCombat(GamePlayer& player, MonsterCard& monster, GameInstance& game)
 {
 	string playerName = player.getPlayerName();
 
-	cout << playerName << " is fighting " << monster.getName() << "!\n";
-	cout << ">> " << monster.getDescription() << "\n\n"; // description of the monster
+	// Announce the start of the fight with a slight delay
+	std::cout << playerName << " is fighting " << monster.getName() << "!\n";
+	std::this_thread::sleep_for(std::chrono::seconds(1));  // 1-second delay for dramatic effect
+	std::cout << ">> " << monster.getDescription() << "\n\n";  // description of the monster
+	std::this_thread::sleep_for(std::chrono::seconds(1));  // Another small delay
 
 	int basePlayerPower = player.getPlayerPower();
 	int baseMonsterPower = monster.getLevel();
@@ -24,13 +29,27 @@ void CombatManager::resolveCombat(GamePlayer& player, MonsterCard& monster, Game
 	int totalPlayerPower = basePlayerPower + playerRoll;
 	int totalMonsterPower = baseMonsterPower + monsterRoll;
 
+	// Announce the rolls with a slight pause
+	std::cout << playerName << " rolls a d20...\n";
+	std::this_thread::sleep_for(std::chrono::seconds(2));  // 2-second delay for suspense
+	std::cout << playerName << " rolled a " << playerRoll << "!\n";
+	std::this_thread::sleep_for(std::chrono::seconds(2));
 	std::cout << "-> Player Power: " << basePlayerPower << " + d20 (" << playerRoll << ") = " << totalPlayerPower << "\n";
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::cout << monster.getName() << " rolls a d20...\n";
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::cout << monster.getName() << " rolled a " << monsterRoll << "!\n";
+	std::this_thread::sleep_for(std::chrono::seconds(2));
 	std::cout << "-> Monster Power: " << baseMonsterPower << " + d20 (" << monsterRoll << ") = " << totalMonsterPower << "\n";
+	std::this_thread::sleep_for(std::chrono::seconds(2));  // 2-second delay for suspense
 
 	if (totalPlayerPower >= totalMonsterPower) { // Victory
 		cout << playerName << " defeated the monster!\n";
+		std::this_thread::sleep_for(std::chrono::seconds(1));  // Pause for dramatic effect
+
 		if (!monster.getWinDescription().empty()) {
 			std::cout << ">> " << monster.getWinDescription() << "\n";
+			std::this_thread::sleep_for(std::chrono::seconds(1));  // Pause after win description
 		}
 		// Apply effects for winning
 
@@ -39,8 +58,12 @@ void CombatManager::resolveCombat(GamePlayer& player, MonsterCard& monster, Game
 		}
 	} else { // Defeat
 		cout << playerName << " was defeated by the monster!\n";
+		std::this_thread::sleep_for(std::chrono::seconds(1));  // Pause for dramatic effect
+
 		if (!monster.getLoseDescription().empty()) {
 			std::cout << ">> " << monster.getLoseDescription() << "\n";
+			std::this_thread::sleep_for(std::chrono::seconds(3));  // Pause after lose description
+			std::cout << endl;
 		}
 
 		const auto& loseEffects = monster.getLoseEffects();
@@ -56,7 +79,9 @@ void CombatManager::resolveCombat(GamePlayer& player, MonsterCard& monster, Game
 			std::uniform_int_distribution<> dist(1, 6);
 			int roll = dist(gen);
 
-			std::cout << playerName << " attempts to escape! Needs " << requiredRoll << "+... Rolled a " << roll << ".\n";
+			std::cout << playerName << " attempts to escape! Needs a " << requiredRoll << "!!!" << endl;
+			std::this_thread::sleep_for(std::chrono::seconds(2));  // Dramatic pause before escape roll result
+			std::cout << playerName << " rolled a " << roll << ".\n";
 
 			if (roll >= requiredRoll) {
 				std::cout << playerName << " escapes successfully with a " << roll << "!\n";
@@ -64,6 +89,7 @@ void CombatManager::resolveCombat(GamePlayer& player, MonsterCard& monster, Game
 			}
 			else {
 				std::cout << playerName << " fails to escape with a " << roll << "!\n";
+				std::this_thread::sleep_for(std::chrono::seconds(2));  // Pause after escape failure
 			}
 		}
 
